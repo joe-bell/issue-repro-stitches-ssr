88ab8f767e5073ec5bebceb8d9656858bb681d8b
joe-bell <joe@joebell.co.uk>
2021-02-17 11:40:07 +0200

    fix: adjust getCssString technique

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,27 +3,14 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { getCssString } from "../stitches.config";
 
 export default class Document extends NextDocument {
-  static async getInitialProps(ctx) {
-    try {
-      const initialProps = await NextDocument.getInitialProps(ctx);
-
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            <style id="stitches">{getCssString()}</style>
-          </>
-        ),
-      };
-    } finally {
-    }
-  }
-
   render() {
+    const styles = getCssString();
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <style id="stitches" dangerouslySetInnerHTML={{ __html: styles }} />
+        </Head>
         <body>
           <Main />
           <NextScript />
